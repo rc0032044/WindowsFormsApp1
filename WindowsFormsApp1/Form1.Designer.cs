f311370261ebb62dae9449f74a03edfb19cfacd9
Ryan Farrell <rc0032044@student.riversidecollege.ac.uk>
2018-09-18 10:32:58 +0100

    added a button to move pictureBox1 in a square triangle and a circle

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,20 +28,22 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.btn1 = new System.Windows.Forms.Button();
+			this.btnsquare = new System.Windows.Forms.Button();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.btntri = new System.Windows.Forms.Button();
+			this.btncircle = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// btn1
+			// btnsquare
 			// 
-			this.btn1.Location = new System.Drawing.Point(69, 367);
-			this.btn1.Name = "btn1";
-			this.btn1.Size = new System.Drawing.Size(75, 23);
-			this.btn1.TabIndex = 0;
-			this.btn1.Text = "button1";
-			this.btn1.UseVisualStyleBackColor = true;
-			this.btn1.Click += new System.EventHandler(this.button1_Click);
+			this.btnsquare.Location = new System.Drawing.Point(69, 367);
+			this.btnsquare.Name = "btnsquare";
+			this.btnsquare.Size = new System.Drawing.Size(75, 23);
+			this.btnsquare.TabIndex = 0;
+			this.btnsquare.Text = "square";
+			this.btnsquare.UseVisualStyleBackColor = true;
+			this.btnsquare.Click += new System.EventHandler(this.btnsquare_Click);
 			// 
 			// pictureBox1
 			// 
@@ -54,13 +56,35 @@
 			this.pictureBox1.TabStop = false;
 			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
 			// 
+			// btntri
+			// 
+			this.btntri.Location = new System.Drawing.Point(607, 367);
+			this.btntri.Name = "btntri";
+			this.btntri.Size = new System.Drawing.Size(75, 23);
+			this.btntri.TabIndex = 5;
+			this.btntri.Text = "triangle";
+			this.btntri.UseVisualStyleBackColor = true;
+			this.btntri.Click += new System.EventHandler(this.btntri_Click);
+			// 
+			// btncircle
+			// 
+			this.btncircle.Location = new System.Drawing.Point(353, 367);
+			this.btncircle.Name = "btncircle";
+			this.btncircle.Size = new System.Drawing.Size(75, 23);
+			this.btncircle.TabIndex = 6;
+			this.btncircle.Text = "circle";
+			this.btncircle.UseVisualStyleBackColor = true;
+			this.btncircle.Click += new System.EventHandler(this.btncircle_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btncircle);
+			this.Controls.Add(this.btntri);
 			this.Controls.Add(this.pictureBox1);
-			this.Controls.Add(this.btn1);
+			this.Controls.Add(this.btnsquare);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -70,8 +94,10 @@
 
 		#endregion
 
-		private System.Windows.Forms.Button btn1;
+		private System.Windows.Forms.Button btnsquare;
 		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button btntri;
+		private System.Windows.Forms.Button btncircle;
 	}
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -17,12 +17,27 @@ namespace WindowsFormsApp1
 			InitializeComponent();
 		}
 
-		private async void button1_Click(object sender, EventArgs e)
+		private async void btnsquare_Click(object sender, EventArgs e)
 		{
-			for(int i = 0; i < 20; i++)
+			for (int i = 0; i < 50; i++)
 			{
-				pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y);
-				await Task.Delay(50);
+				pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+				await Task.Delay(25);
+			}
+			for (int i = 0; i < 50; i++)
+			{
+				pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
+				await Task.Delay(25);
+			}
+			for (int i = 0; i < 50; i++)
+			{
+				pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+				await Task.Delay(25);
+			}
+			for (int i = 0; i < 50; i++)
+			{
+				pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
+				await Task.Delay(25);
 			}
 		}
 
@@ -30,5 +45,41 @@ namespace WindowsFormsApp1
 		{
 
 		}
+
+		private async void btntri_Click(object sender, EventArgs e)
+		{
+			for (int i = 0; i < 14; i++)
+			{
+				pictureBox1.Location = new Point(pictureBox1.Location.X + 15, pictureBox1.Location.Y);
+				await Task.Delay(25);
+			}
+			pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y);
+
+			for (int i = 0; i < 10; i++)
+			{
+				pictureBox1.Location = new Point(pictureBox1.Location.X - 11, pictureBox1.Location.Y - 11);
+				await Task.Delay(25);
+			}
+			for (int i = 0; i < 10; i++)
+			{
+				pictureBox1.Location = new Point(pictureBox1.Location.X - 11, pictureBox1.Location.Y + 11);
+				await Task.Delay(25);
+			}
+		}
+
+		private async void btncircle_Click(object sender, EventArgs e)
+		{
+			int r = 100;
+			int x = pictureBox1.Location.X - r;
+			int y = pictureBox1.Location.Y;
+			
+			for (double a = 0; a < Math.PI * 2; a = a + (Math.PI / 180.0))
+			{
+				pictureBox1.Location = new Point((int)(x + r * Math.Cos(a)), (int)(y + r * Math.Sin(a)));
+				await Task.Delay(10);
+			}
+		}
+
 	}
+		
 }
